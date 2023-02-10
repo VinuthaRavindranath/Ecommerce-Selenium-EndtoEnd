90774149e845ce8530e88fc26d0bdd1ad3c42637
Vinutha R <vinuthar@Vinuthas-MacBook-Pro.local>
2023-02-10 22:21:16 +0530

    FirstCommit

---
+++ src/main/java/LambdaTest/Ecomm/App.java
@@ -0,0 +1,13 @@
+package LambdaTest.Ecomm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
